4523c3d814a508a99a7cd1b3a11f567f76c32438
spoletta87 <44589517+spoletta87@users.noreply.github.com>
2018-10-29 22:56:40 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="it">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <link href="css/style.css" rel="stylesheet">
+    <link rel="shortcut icon" href="images/logo.png">
+    <meta name="author" content="Massimo Frau">
+    <meta name="description" content=" Personal Trainer allenamenti personalizzati cura della persona">
+  </head>
+  <body>
+    <!-- Header -->
+    <header class="container-fluid bg-white h-100 clearfix p-0 ">
+     
+      <nav class=" navbar navbar-expand-lg navbar-light nav-bg pl-5">
+        <h1 class="titolo display-5"> 
+        A.S.D. Studio Anteo</h1>
+       <a class="navbar-brand name="studio anteo"><img src="images/livello.png"> </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon "></span>
+        </button>
+       
+        <ul class="justify-content-around navbar-nav collapse navbar-collapse " id="navbarNavDropdown">
+          <li class="nav-item">
+            <a class="nav-link" href="#"><strong>Il nostro Team</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><strong>La nostra missione</strong></a>
+          </li>
+      
+          <li class="nav-item">
+            <a class="nav-link " href="#"><strong>La nostra arena</strong></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#"><strong>Come trovarci</strong></a>
+          </li>
+
+            
+        
+
+
+        </ul>
+      </nav>
+
+    </header>
+
+    <!-- Gallery + Logo -->
+ 
+    <div class="container-fluid p-0 ">
+      <div class="row m-0">
+      <div class="col-sm-2"><h1 class="d-none">ASD ANTEO</h1><img src="images/logo.png" class="shoulder-img d-flex"></div>
+
+
+      <div id="carouselExampleSlidesOnly" class="carousel slide  gallery col-sm-8 " data-ride="carousel">
+        <div class="carousel-inner w-100 m-auto">
+          <div class="carousel-item active ">
+            <img class="d-block w-100 h-100" src="images/palestra.jpg?auto=yes&bg=777&fg=555&text=First slide" alt="First slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100 h-100" src="images/palestra1.jpg?auto=yes&bg=666&fg=444&text=Second slide" alt="Second slide">
+          </div>
+          <div class="carousel-item">
+            <img class="d-block w-100 h-100" src="images/palestra2.jpg?auto=yes&bg=555&fg=333&text=Third slide" alt="Third slide">
+          </div>
+        </div>
+        
+        <div class="assoluta w-100" style="height: inherit;">
+          
+        <!--   <img src="images/Studio_Anteo.png" class=" d-flex m-auto logo-centro  p-5"  > -->
+        </div>
+      </div>   
+
+      <div class="col-sm-2"><img src="images/logo.png" class="shoulder-img d-flex"></div>
+
+  <a href="https://www.freepik.com/free-photos-vectors/background " class="d-none">Background vector created by Starline - Freepik.com</a>
+</div>
+    </div>
+  </body>
+</html>
+
+
+
+
+<div class="container m-auto">
+    <div class="row m-5 ">
+      <div class="col-7 m-auto bg-yellow p-3">
+        <h2 class="display-4 text-center">IL PERSONAL TRAINER</h2>
+          <img src="images/coach.jpg" class="float-left w-25 p-3">
+          <p>Oggi come oggi si sente parlare abbastanza spesso della figura del <strong>personal trainer.</strong><br>Molte persone si affidano ad un trainer personale che si possa occupare del loro allenamento e della loro forma fisica, dello svolgimento di attività sportive e della personalizzazione del proprio training.<br>Il personal trainer non è un soggetto improvvisato, al contrario è una figura altamente specializzata e con una formazione che non è solamente di natura sportiva, ma anche di salute alimentare e di educazione alla salute. Infatti il fitness trainer ha una prospettiva a tutto tondo sul corpo umano, su come funziona e su come farlo stare meglio. Non si tratta semplicemente della figura che si occupa di predisporre una serie di allenamenti personalizzati allo scopo di far raggiungere una determinata forma fisica al cliente: il personal trainer fa anche questo, ma il suo lavoro è molto più complesso perché consiste appunto nel guidare il cliente verso uno stato fisico e di benessere ottimale per le sue condizioni.<br>Ecco perché il fitness coach è innanzitutto una persona con un certo bagaglio culturale alle spalle.</p>
+      </div>
+    </div>
+</div>
+
+
+
+
+
+<div class="container m-auto">
+  <div class="row m-5">
+    <div class="col6 m-auto">
+   <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Fmassimo.frau.3950%2Fposts%2F299051760642505&width=500"  margin-left="20px" width="500" height="567" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" class="pr-3" ></iframe>
+    </div>
+
+    <div class="col6 m-auto">
+      <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ffornaio.dicastelgandolfo.7%2Fposts%2F1561623843983242&width=500" width="500" height="174" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media" class="mb-5"></iframe>
+      <br>
+      <iframe src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2Ffornaio.dicastelgandolfo.7%2Fposts%2F1561623843983242&width=500" width="500" height="174" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+      
+    </div>
+  </div>  
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
